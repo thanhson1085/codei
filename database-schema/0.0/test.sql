fe1b0e817217809035e4862f894298a73357ff0d
Nguyen Sy Thanh Son <son.nguyen@mobileembrace.com>
2013-07-18 16:21:28 +0700

    deploy script

---
+++ database-schema/0.0/test.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS `test` (
+  `id` tinyint(4) NOT NULL AUTO_INCREMENT,
+  `name` varchar(250) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
--- deploy-scripts/devlopment.sh
+++ deploy-scripts/devlopment.sh
@@ -1,7 +1,36 @@
 #!bin/bash
 
 echo 'Starting...'
+expected_args=3
+e_badargs=65
+ 
+if [ $# -ne $expected_args ]
+then
+  echo "Error: you should use command $0 version dbuser dbpass"
+  exit $e_badargs
+fi
+
 chown -R www-data:www-data ../application/cache
 chown -R www-data:www-data ../application/logs
-echo 'DONE!!!'
+cd ../application/config/
+for config_file in *.dist
+do
+	cp $config_file ${config_file%.*}
+done
 
+
+cd ../../
+database_name=${PWD##*/}
+
+cd database-schema
+if [ -d $1 ]
+then
+	cd $1
+	echo "Running SQL Script..."
+	mysql -u$2 -p$3 -e "CREATE DATABASE IF NOT EXISTS $database_name"
+	for schema_file in *.sql
+	do
+		mysql -u$2 -p$3 $database_name < ${PWD}/$schema_file
+	done
+fi
+echo 'DONE!!!'
