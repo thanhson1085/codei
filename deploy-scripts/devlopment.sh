c134f9a6a2830fb03cbfe8158d1a8277eebfb2a7
Nguyen Sy Thanh Son <son.nguyen@mobileembrace.com>
2013-07-30 12:11:07 +0700

    rollback update

--- deploy-scripts/devlopment.sh
+++ deploy-scripts/devlopment.sh
@@ -8,20 +8,21 @@ current_time=$(date "+%Y.%m.%d-%H.%M.%S")
  
 if [ $# -ne $expected_args ]
 then
-  echo "Error: you should use command $0 version dbuser dbpass"
-  exit $e_badargs
+	echo "Error: you should use command $0 version dbuser dbpass"
+	exit $e_badargs
 fi
 
 chown -R www-data:www-data ../application/cache
 chown -R www-data:www-data ../application/logs
 
 cd $home_dir
-echo "Running update source code ..."
+echo "Running update source code from DEV branch..."
 git pull origin master
 
 cd application/config/
-for config_file in *.dist
+for config_file in *.development
 do
+	echo "Genarating ${config_file%.*}..."
 	cp $config_file ${config_file%.*}
 done
 
@@ -30,15 +31,16 @@ cd $home_dir
 database_name=${PWD##*/}
 
 mysql -u$2 -p$3 -e "CREATE DATABASE IF NOT EXISTS $database_name"
-echo "Backup Database..."
+echo "Backup Database $database_name..."
+rm -f $home_dir/database-backups/*.sql
 mysqldump -u$2 -p$3 $database_name > $home_dir/database-backups/$current_time.sql
 cd database-schema
 if [ -d $1 ]
 then
 	cd $1
-	echo "Running SQL Script..."
 	for schema_file in *.sql
 	do
+		echo "Running SQL Script $schema_file..."
 		mysql -u$2 -p$3 $database_name < ${PWD}/$schema_file
 	done
 fi
--- deploy-scripts/rollback.sh
+++ deploy-scripts/rollback.sh
@@ -1,6 +1,20 @@
 #!bin/bash
 home_dir=$PWD/../
+expected_args=2
+e_badargs=65
+if [ $# -ne $expected_args ]
+then
+	echo "Error: you should use command $0 dbuser dbpass"
+	exit $e_badargs
+fi
 cd $home_dir
-echo "Starting"
+database_name=${PWD##*/}
+echo "Starting..."
+echo "Rollback source code..."
 git reset --hard HEAD@{1}
+for backup_file in database-backups/*.sql
+do
+	echo "Rollback database $database_name..."
+	mysql -u$1 -p$2 $database_name < $backup_file
+done
 echo "DONE"
